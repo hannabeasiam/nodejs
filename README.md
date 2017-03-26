e52669a3f4b01ca59139f1604e53b5affbf2f1a3
hanna lee <hannabeasiam@gmail.com>
2017-03-26 19:36:08 -0400

    second commit_read html file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 This is first commit for Nodejs "Hello World"
+
+read html file from node.js
+
+app.js : using specified server
+server.js : using local host
+
--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const http = require('http');
-
+const fs = require('fs');
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n I earned $50');
+  res.setHeader('Content-Type', 'text/html');
+  fs.readFile('./index.html',null, function(error, data) {
+    if(error){
+      res.writeHead(404);
+      res.write('File not found!');
+    }
+    else{
+      res.write(data);
+    }
+  res.end();
+    });
 });
 
 server.listen(port, hostname, () => {
